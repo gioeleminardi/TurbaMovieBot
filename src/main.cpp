2d994c7527bf9339e1aa17c8a83886939c2ff80c
Gioele Minardi <gioele.minardi@elt.it>
2021-05-03 15:42:21 +0200

    Added dependencies
    - sqlite3 3.35.5
    - tgbot-cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * \file       main.cpp
+ * \author     Gioele Minardi <gioelem3@gmail.com>
+ * \copyright  (C) 2021
+ * \license    see LICENSE.txt
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello\n";
+    return 0;
+}
